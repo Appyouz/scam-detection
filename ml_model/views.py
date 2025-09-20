c152326fcfbc9b49894decb7eeae4070d4e81bd8
appyouz <dikpalkhatri7@gmail.com>
2025-09-20 14:41:38 +0545

    feat: add post request view

--- ml_model/views.py
+++ ml_model/views.py
@@ -1,4 +1,13 @@
 from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
 
+
+@csrf_exempt
 def home(request):
+    if request.method == 'POST':
+        input_text = request.POST.get('email-text', '')
+        print("Received text for analysis: ",  input_text)
+
+        # ML model prediction logic goes here
+
     return render(request, 'ml_model/index.html')
